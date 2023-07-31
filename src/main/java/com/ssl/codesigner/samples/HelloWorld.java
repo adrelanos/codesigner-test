ce5e9abfc3e19e056c1be046341661aba677f29a
Patrick Schleizer <adrelanos@whonix.org>
2023-07-31 12:04:30 -0400

    .

--- src/main/java/com/ssl/codesigner/samples/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.ssl.codesigner.samples;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Happy Signing From ESigner.com!");
-    }
-}
